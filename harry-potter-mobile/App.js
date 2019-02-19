389371e78c8235288d58a141cf2725b59704fdf5
Michael George <mgeorge9492@gmail.com>
2019-02-19 14:39:46 -0800

    updates

--- harry-potter-mobile/App.js
+++ harry-potter-mobile/App.js
@@ -1,21 +1,113 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Torch from 'react-native-torch';
+import {
+  Gyroscope,
+} from 'expo';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
-export default class App extends React.Component {
+export default class GyroscopeSensor extends React.Component {
+  state = {
+    gyroscopeData: {},
+  }
+
+  Torch.switchState(true); // Turn ON
+  Torch.switchState(false); // Turn OFF
+
+  componentDidMount() {
+    this._toggle();
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _toggle = () => {
+    if (this._subscription) {
+      this._unsubscribe();
+    } else {
+      this._subscribe();
+    }
+  }
+
+  _slow = () => {
+    Gyroscope.setUpdateInterval(1000);
+  }
+
+  _fast = () => {
+    Gyroscope.setUpdateInterval(16);
+  }
+
+  _subscribe = () => {
+    this._subscription = Gyroscope.addListener((result) => {
+      this.setState({ gyroscopeData: result });
+    });
+  }
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  }
+
   render() {
+    let { x, y, z } = this.state.gyroscopeData;
+
     return (
-      <View style={styles.container}>
-        <Text>Two Features: Sorting Hat House Sorter and a Lumos "flashlight spell"</Text>
+      <View style={styles.sensor}>
+        <Text>Gyroscope:</Text>
+        <Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this._toggle} style={styles.button}>
+            <Text>Toggle</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._slow} style={[styles.button, styles.middleButton]}>
+            <Text>Slow</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._fast} style={styles.button}>
+            <Text>Fast</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 100) / 100;
+}
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+  sensor: {
+    marginTop: 15,
+    paddingHorizontal: 10,
   },
 });
